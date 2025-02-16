4a9c972f24778fbda66ce0bfe2d736a240203606
pancham01 <pancham9557@gmail.com>
2025-02-16 14:45:40 +0530

    update

--- src/main/java/com/explore/controller/UserController.java
+++ src/main/java/com/explore/controller/UserController.java
@@ -1,20 +1,22 @@
 package com.explore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.explore.model.User;
 import com.explore.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
 	@Autowired
 	private UserService userService;
 	
 	@PostMapping
+	@ResponseBody
 	public User addUser(@RequestBody User user)
 	{
 		System.out.println("UserController.addUser()");
@@ -22,4 +24,11 @@ public class UserController {
 		return user;
 	}
 
+	
+	@GetMapping("/")
+	public String get()
+	{
+		System.out.println("UserController.get()");
+		return "index";
+	}
 }
