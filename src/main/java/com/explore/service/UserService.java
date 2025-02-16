d7693945803f748c3658fc3842697301974c75f1
pancham01 <pancham9557@gmail.com>
2025-02-16 17:28:35 +0530

    update

--- src/main/java/com/explore/service/UserService.java
+++ src/main/java/com/explore/service/UserService.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Service;
 import com.explore.dao.UserDao;
 import com.explore.model.User;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class UserService {
 	@Autowired
 	private UserDao userDao;
 	
+	@Transactional
 	public void addUser(User user) {
 		System.out.println("UserService.addUser()");
 		userDao.addUser(user);
